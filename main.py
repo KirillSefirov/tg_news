723bbfe205a222ca14436d8c590cfbba5f048e41
Dmitry Karasev <dmitrykarasev@Dmitrys-MacBook-Pro.local>
2025-05-10 15:42:39 +0300

    finished getting article body

--- main.py
+++ main.py
@@ -21,17 +21,26 @@ def get_article_general_data_from_html(article_full_html):
     art_data = article_full_html.find("a", class_="loop-card__title-link")
     art_url = art_data.get_attribute_list("href").pop().strip()
     art_title = art_data.text
-    art_datetime = datetime.fromisoformat(article_full_html.find("time").get_attribute_list("datetime").pop()).date()
+    try:
+        art_datetime = datetime.fromisoformat(article_full_html.find("time").get_attribute_list("datetime").pop()).date()
+    except AttributeError:
+        art_datetime = None
     return {"article_url": art_url, "article_title": art_title, "article_date": art_datetime}
 
-# parsed_art_data = [get_article_general_data_from_html(art) for art in articles]
-    # print(get_article_general_data_from_html(art))
+parsed_art_data = [get_article_general_data_from_html(art) for art in articles]
 
-# def get_article_body_by_url(art_url):
-#     article_html = requests.get
-# list_of_articles = []
-# for art in articles:
-#     print(get_article_general_data_from_html(art))
+def get_article_body_by_url(art_url):
+    article_html = BeautifulSoup(requests.get(art_url).text, "html.parser")
+    all_paragraphs_html = article_html.find_all("p", class_="wp-block-paragraph")
+    parsed_pars = []
+    for par in all_paragraphs_html:
+        parsed_pars.append(par.text)
+    return parsed_pars
 
-send_telegram("alalal")
+for i in range(len(parsed_art_data)):
+    parsed_art_data[i]["article_body"] = get_article_body_by_url(parsed_art_data[i]["article_url"])
+    time.sleep(0.1)
+
+
+# send_telegram("alalal")
 
